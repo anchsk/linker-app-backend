8a11069b33cc1bee8c68eb3dade8cfc785ffff40
Anna <46578108+anchsk@users.noreply.github.com>
2020-10-11 22:34:32 +0200

    build ui

---
+++ build/precache-manifest.cf238c090c13efca470a247bdc296ce9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e674d63b30393d718523e0f050a6d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b1cc21792f834b92c61",
+    "url": "/static/css/main.6a54cfde.chunk.css"
+  },
+  {
+    "revision": "12399554ee52f2b6cb8b",
+    "url": "/static/js/2.ec1a5a7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec1a5a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1cc21792f834b92c61",
+    "url": "/static/js/main.87e13039.chunk.js"
+  },
+  {
+    "revision": "78da516ed32c12b7ad5a",
+    "url": "/static/js/runtime-main.f411919f.js"
+  },
+  {
+    "revision": "367e94a67909fbeb72e5db2a3214ee95",
+    "url": "/static/media/ArrowDown.367e94a6.svg"
+  },
+  {
+    "revision": "b9b596c7b7dd3e59d6b8ed6a2ad4d5be",
+    "url": "/static/media/Dots.b9b596c7.svg"
+  },
+  {
+    "revision": "f7803d6671aa30520028fb48dcd74044",
+    "url": "/static/media/FaceCircle.f7803d66.svg"
+  },
+  {
+    "revision": "9093ed48ecb21f84a300f786e5a111d2",
+    "url": "/static/media/Heart.9093ed48.svg"
+  },
+  {
+    "revision": "4e2e86733ef877fed3bfd4a109fdd60c",
+    "url": "/static/media/Inter-Light.4e2e8673.woff2"
+  },
+  {
+    "revision": "8070997696b1c91905fecba458f96640",
+    "url": "/static/media/Inter-Regular.80709976.woff2"
+  },
+  {
+    "revision": "a553f44ac2c2f8ffdb1290d33b7c2425",
+    "url": "/static/media/Inter-SemiBold.a553f44a.woff2"
+  },
+  {
+    "revision": "d8da461164d1c4e2f61a6d8d750dd76f",
+    "url": "/static/media/LinkerText.d8da4611.svg"
+  },
+  {
+    "revision": "efb3ae8b938405544949fcbc044c53d5",
+    "url": "/static/media/OneStar.efb3ae8b.svg"
+  },
+  {
+    "revision": "41880490ecf459b52cc68695c718756a",
+    "url": "/static/media/Plus.41880490.svg"
+  },
+  {
+    "revision": "aa2753a730679da76b3afb260afe1f14",
+    "url": "/static/media/StarLogoRounded.aa2753a7.svg"
+  },
+  {
+    "revision": "59351e780d0799f9856006d700833523",
+    "url": "/static/media/ThreeStars.59351e78.svg"
+  },
+  {
+    "revision": "9a39559da9cbdbfb0853ae3ecc9d557b",
+    "url": "/static/media/X.9a39559d.svg"
+  }
+]);
